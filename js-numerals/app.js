7b79632a94e5f2ff5fcc6b55c8e7504edff18250
AndrasGardos <gardosandras@gmail.com>
2020-01-28 16:59:43 +0100

    refactor app, add error checks

--- js-numerals/app.js
+++ js-numerals/app.js
@@ -1,17 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById("number-to-convert").oninput = handleNumberChange;
+  document.getElementById("number-input").oninput = handleNumberChange;
 });
 
 function handleNumberChange(e) {
-  const numberInput = document.getElementById("number-to-convert");
-  const textOutput = document.getElementById("converted-number");
-  const convertedNumber = convertNumber(numberInput.value);
+  const numberInput = document.getElementById("number-input");
+  const textOutput = document.getElementById("converted-number-output");
+  const numberToConvert = parseInt(numberInput.value);
 
-  if (convertedNumber) {
-    textOutput.style.color = "black";
-    textOutput.innerText = convertedNumber;
-  } else {
+  if (!Number.isInteger(numberToConvert)) {
     textOutput.style.color = "red";
     textOutput.innerHTML = "Please enter a valid integer!";
+    return;
+  }
+
+  if (!Number.isSafeInteger(numberToConvert)) {
+    textOutput.style.color = "red";
+    textOutput.innerHTML =
+      "This integer is way too big! <br> Limits: ± 9 007 199 254 740 991";
+    return;
+  }
+
+  try {
+    const convertedNumber = convertNumber(numberToConvert);
+    textOutput.style.color = "black";
+    textOutput.innerText = convertedNumber;
+  } catch (error) {
+    textOutput.style.color = "red";
+    textOutput.innerHTML =
+      "Some nexpected error happened while converting your integer :(";
   }
 }
