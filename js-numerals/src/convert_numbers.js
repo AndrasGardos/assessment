ae2ef23d2dffb1eeea1d7466081381adf56d2ff8
AndrasGardos <gardosandras@gmail.com>
2020-01-28 12:40:14 +0100

    add logic for two digit numbers

--- js-numerals/src/convert_numbers.js
+++ js-numerals/src/convert_numbers.js
@@ -11,8 +11,46 @@ const DIGITS = [
   "nine"
 ];
 
+const TEN_TO_NINETEEN = [
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen"
+];
+
+const MULTIPLES_OF_TEN = [
+  "",
+  "ten",
+  "twenty",
+  "thirty",
+  "fourty",
+  "fifty",
+  "sixty",
+  "seventy",
+  "eighty",
+  "ninety"
+];
+
 function convertNumbers(number) {
-  return DIGITS[number];
+  if (number >= 0 && number <= 9) {
+    return DIGITS[number];
+  } else if (number >= 10 && number <= 19) {
+    return TEN_TO_NINETEEN[number - 10];
+  } else if (number >= 20 && number <= 99) {
+    if (number % 10 == 0) {
+      return MULTIPLES_OF_TEN[number / 10];
+    } else {
+      return (
+        MULTIPLES_OF_TEN[Math.floor(number / 10)] + "-" + DIGITS[number % 10]
+      );
+    }
+  }
 }
 
 module.exports = {
--- js-numerals/test/test_convert_numbers.js
+++ js-numerals/test/test_convert_numbers.js
@@ -9,4 +9,22 @@ describe("Test convertNumbers function", function() {
       assert.equal(convertNumbers(9), "nine");
     });
   });
+
+  describe("Test for double digit numbers", function() {
+    it("should convert 10", function() {
+      assert.equal(convertNumbers(10), "ten");
+    });
+    it("should convert 12", function() {
+      assert.equal(convertNumbers(12), "twelve");
+    });
+    it("should convert 17", function() {
+      assert.equal(convertNumbers(17), "seventeen");
+    });
+    it("should convert 42", function() {
+      assert.equal(convertNumbers(42), "fourty-two");
+    });
+    it("should convert 50", function() {
+      assert.equal(convertNumbers(50), "fifty");
+    });
+  });
 });
